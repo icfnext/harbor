34f7726e5360548ba436bb7f124fbbea31c52889
Matthew Young <myoung@citytechinc.com>
2014-01-03 12:00:37 -0600

    Removal of unused javascript

--- harbor-ui/src/main/content/jcr_root/libs/harbor/components/content/fluidcolumnrow/clientlibs/fluidColumnRow.js
+++ harbor-ui/src/main/content/jcr_root/libs/harbor/components/content/fluidcolumnrow/clientlibs/fluidColumnRow.js
@@ -18,45 +18,6 @@ Harbor.Components.FluidColumnRow = function(jQuery){
 
 
     return {
-        moveColumnLeft: function() {
-            Harbor.Components.FluidColumnRow.moveColumn.call( this, -1 );
-        },
-
-        moveColumnRight: function() {
-            Harbor.Components.FluidColumnRow.moveColumn.call( this, 1 );
-        },
-
-        moveColumn: function( direction ) {
-            var curIndex = jQuery( this.element.dom.parentElement.parentElement ).index();
-
-            var newIndex = curIndex + ( 1 * direction );
-
-            var childCount = jQuery( this.element.dom.parentElement.parentElement.parentElement ).children().length;
-
-            if ( newIndex < 0 ) {
-                newIndex = 0;
-            }
-            if ( newIndex >= childCount ) {
-                newIndex = childCount - 1;
-            }
-
-            if ( curIndex != newIndex ) {
-                var itemPath = this.path;
-                var currentEditable = this;
-
-                jQuery.post(
-                    this.path,
-                    {
-                        ":order" : newIndex
-                    },
-                    function( data ) {
-                        console.log ( itemPath + " :: Order Updated" );
-                        currentEditable.refreshParent();
-                    }
-                );
-            }
-        },
-
         addColumn: function(editableContext){
             sendColumnAddPost(editableContext.path + '/*', column,
                 function( data ) { editableContext.refreshSelf() });
