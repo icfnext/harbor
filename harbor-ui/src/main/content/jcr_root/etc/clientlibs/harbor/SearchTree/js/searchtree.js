b3b0faa43dd0560d00f3c4cf51dcd13c60aad527
Matthew Young <myoung@citytechinc.com>
2014-01-13 15:24:30 -0600

    progress on blocking out a search function using a parameterized search config object

--- harbor-ui/src/main/content/jcr_root/etc/clientlibs/harbor/SearchTree/js/searchtree.js
+++ harbor-ui/src/main/content/jcr_root/etc/clientlibs/harbor/SearchTree/js/searchtree.js
@@ -11,7 +11,17 @@ Harbor.Overrides.SearchTree = function(){
 
 
     var defaultSearchConfiguration = function(){
+        return {
+            searchFunction: function(node){
 
+            },
+            searchResultBuilder: function(node){
+                return node;
+            },
+            pruneBranch: function(node){
+
+            }
+        }
     }();
 
     function SearchTree(node){
@@ -22,47 +32,57 @@ Harbor.Overrides.SearchTree = function(){
         this.root = node;
     }
 
-    function buildSearchTree(path){
-        var subExpressions = path.split("/");
-        var root = new Node();
-        var priorDepth = []
-        priorDepth.push(root);
-
-        for(var i = 0; i < subExpressions.length; i++){
-            var currentDepth = [];
-            var curSubSplit = subExpressions[i].split("|");
-
-            for(var j = 0; j < priorDepth.length; j++){
-                //loop through the split sub expressions in reverse
-                for(var k = curSubSplit.length - 1; k >= 0; k--){
-                    var expressionNode = new Node({expression: curSubSplit[k]});
-                    currentDepth.push(expressionNode);
-                    priorDepth[j].children.push(expressionNode);
-                }
-            }
-            priorDepth = currentDepth;
-        }
-        return new SearchTree(root);
-    }
-
-
     return {
-        SearchTree: function SearchTree(path){
-            //Force the 'new' keyword when creating a search tree
-            if(! (this instanceof SearchTree))
-                return new SearchTree(path);
+        buildSearchTree: function(path){
+            var subExpressions = path.split("/");
+            var root = new Node();
+            var priorDepth = [];
+            priorDepth.push(root);
 
-            //instance variables
-            this.path = path;
+            for(var i = 0; i < subExpressions.length; i++){
+                var currentDepth = [];
+                var curSubSplit = subExpressions[i].split("|");
 
-            this.tree = buildSearchTree(this.path);
-
+                for(var j = 0; j < priorDepth.length; j++){
+                    //loop through the split sub expressions in reverse
+                    for(var k = curSubSplit.length - 1; k >= 0; k--){
+                        var expressionNode = new Node({expression: curSubSplit[k]});
+                        currentDepth.push(expressionNode);
+                        priorDepth[j].children.push(expressionNode);
+                    }
+                }
+                priorDepth = currentDepth;
+            }
+            return new SearchTree(root);
         },
 
+        search: function(searchTree, options){
+            options = options || {};
+            var searchConf = options.searchConfiguration || defaultSearchConfiguration;
+            var toSearch = [];
+            var searchResults = [];
+            toSearch.push(searchTree.root);
 
+            while(toSearch.length > 0){
+                var currentNode = toSearch.shift();
 
+                /*
+                    Apply provided Search configuration logic.
+                 */
+                if(searchConf.searchFunction(currentNode)){
+                    if (searchConf.searchResultBuilder instanceof Function){
+                        searchResults.push(searchConf.searchResultBuilder(currentNode));
+                    }
+                }
+                if((!searchConf.pruneBranch instanceof Function) || searchConf.pruneBranch(currentNode) == false){
+                    for(var i = 0; i < currentNode.children.length; i++){
+                        toSearch.push(currentNode.children[i]);
+                    }
+                }
 
+            }
 
+        }
 
     }
 }();
