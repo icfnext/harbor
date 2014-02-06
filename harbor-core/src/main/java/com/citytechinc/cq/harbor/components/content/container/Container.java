6c8eb21fea3e29ef07fbdd0b87d0635e95aa702d
Paul Michelotti <pmichelotti@citytechinc.com>
2014-02-06 16:17:29 -0600

    Header and Footer

--- harbor-core/src/main/java/com/citytechinc/cq/harbor/components/content/container/Container.java
+++ harbor-core/src/main/java/com/citytechinc/cq/harbor/components/content/container/Container.java
@@ -5,11 +5,15 @@ import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.Option;
 import com.citytechinc.cq.component.annotations.widgets.Selection;
 import com.citytechinc.cq.harbor.constants.bootstrap.Bootstrap;
+import com.citytechinc.cq.harbor.constants.dom.Elements;
 import com.citytechinc.cq.library.components.AbstractComponent;
 import com.citytechinc.cq.library.content.request.ComponentRequest;
+import com.google.common.base.Optional;
 
 @Component( value = "Container", description = "A container in which content may be placed.  All content should be placed in a container element.", name = "contentcontainer" )
 public class Container extends AbstractComponent {
+
+    public static final String RESOURCE_TYPE = "harbor/components/content/contentcontainer";
 
     public static final String FULL_WIDTH_PROPERTY = "fullWidth";
 
@@ -35,6 +39,22 @@ public class Container extends AbstractComponent {
 
     }
 
+    public String getContainerElement() {
+        return Elements.DIV;
+    }
+
+    public Optional<String> getRoleOptional() {
+        return Optional.absent();
+    }
+
+    public Boolean getHasRole() {
+        return getRoleOptional().isPresent();
+    }
+
+    public String getRole() {
+        return getRoleOptional().get();
+    }
+
     protected String getContainerFullWidthClass() {
         return Bootstrap.CONTAINER_FULL_WIDTH_CLASS;
     }
@@ -42,5 +62,7 @@ public class Container extends AbstractComponent {
     protected String getContainerDefaultClass() {
         return Bootstrap.CONTAINER_CLASS;
     }
+
+
 
 }
