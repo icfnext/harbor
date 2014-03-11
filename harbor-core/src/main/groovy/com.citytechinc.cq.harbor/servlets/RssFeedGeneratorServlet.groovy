4f93db7f7f6d9e66a22523e3baa269fc430cce3b
Cresten St.Clair <crestenn@gmail.com>
2014-03-11 16:47:56 -0500

    Proto-servlet written, parses request parameter, parses the URLS of the request parameters, and outputs a JSON map from the RSS XML

--- harbor-core/src/main/groovy/com.citytechinc.cq.harbor/servlets/RssFeedGeneratorServlet.groovy
+++ harbor-core/src/main/groovy/com.citytechinc.cq.harbor/servlets/RssFeedGeneratorServlet.groovy
@@ -1,23 +1,41 @@
 package com.citytechinc.cq.harbor.servlets
 
-import com.citytechinc.cq.library.servlets.AbstractJsonResponseServlet
+import com.citytechinc.cq.library.content.request.ComponentServletRequest
+import com.citytechinc.cq.library.servlets.AbstractComponentServlet
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
 import org.apache.felix.scr.annotations.sling.SlingServlet
-import org.apache.sling.api.SlingHttpServletRequest
 import org.apache.sling.api.SlingHttpServletResponse
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 import javax.servlet.ServletException
 
-@SlingServlet(paths = "bin/rssFeed")
-public class RssFeedGeneratorServlet extends AbstractJsonResponseServlet {
-
+@SlingServlet(paths = "/bin/rssFeed")
+public class RssFeedGeneratorServlet extends AbstractComponentServlet {
 
 	@Override
-	protected final void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response)
-	throws ServletException, IOException {
-		def url = "http://news.google.com/news?ned=us&topic=h&output=rss";
-		def rss = new XmlSlurper().parse(url);
-		writeJsonResponse(response, rss);
+	protected final void processGet(final ComponentServletRequest request) throws ServletException, IOException {
+		String rssFeedPathsAsString = request.getRequestParameter("rssFeedPaths").or("");
+		def requestFeedPaths = new JsonSlurper().parseText(rssFeedPathsAsString);
+		def itemList = new ArrayList();
+		requestFeedPaths.each { url ->
+			def rootNode = new XmlParser().parse(url);
+			rootNode.children().each { channel ->
+				channel.children().each { node ->
+					//TODO: make this a constant
+					if (node.name() == "item") {
+						def itemMap = [
+								title: node.title.text(),
+								link: node.link.text(),
+								pubDate: node.pubDate.text(),
+								description: node.description.text()
+						]
+						itemList.add(itemMap);
+					}
+				}
+			}
+		}
+
+		final SlingHttpServletResponse slingResponse = request.getSlingResponse();
+		writeJsonResponse(slingResponse, new JsonBuilder(itemList));
 	}
 }
