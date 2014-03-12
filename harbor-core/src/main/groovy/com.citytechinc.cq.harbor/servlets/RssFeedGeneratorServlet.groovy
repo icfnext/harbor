ec5add1852dd8b3adcf70d1ef0081fc9571de83c
Cresten St.Clair <crestenn@gmail.com>
2014-03-12 11:04:16 -0500

    Sorting working in servlet

--- harbor-core/src/main/groovy/com.citytechinc.cq.harbor/servlets/RssFeedGeneratorServlet.groovy
+++ harbor-core/src/main/groovy/com.citytechinc.cq.harbor/servlets/RssFeedGeneratorServlet.groovy
@@ -8,32 +8,48 @@ import org.apache.felix.scr.annotations.sling.SlingServlet
 import org.apache.sling.api.SlingHttpServletResponse
 
 import javax.servlet.ServletException
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 @SlingServlet(paths = "/bin/rssFeed")
 public class RssFeedGeneratorServlet extends AbstractComponentServlet {
+
+	public static final String ITEM_NODE_NAME = "item";
+	public static final String RSS_FEED_PUBDATE_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
 
 	@Override
 	protected final void processGet(final ComponentServletRequest request) throws ServletException, IOException {
 		String rssFeedPathsAsString = request.getRequestParameter("rssFeedPaths").or("");
 		def requestFeedPaths = new JsonSlurper().parseText(rssFeedPathsAsString);
-		def itemList = new ArrayList();
+		def itemList = [];
 		requestFeedPaths.each { url ->
 			def rootNode = new XmlParser().parse(url);
 			rootNode.children().each { channel ->
 				channel.children().each { node ->
 					//TODO: make this a constant
-					if (node.name() == "item") {
+					if (node.name() == ITEM_NODE_NAME) {
 						def itemMap = [
-								title: node.title.text(),
+								//title: node.title.text(),
 								link: node.link.text(),
 								pubDate: node.pubDate.text(),
-								description: node.description.text()
+								//description: node.description.text()
 						]
 						itemList.add(itemMap);
 					}
 				}
 			}
 		}
+
+		def byPubDateComparator = [
+		        compare: { map1, map2 ->
+			        def dateFormat = new SimpleDateFormat(RSS_FEED_PUBDATE_DATE_FORMAT);
+			        def date1 = dateFormat.parse(map1.pubDate);
+			        def date2 = dateFormat.parse(map2.pubDate);
+			        return date1.compareTo(date2);
+		        }
+		] as Comparator;
+
+		itemList = itemList.sort(byPubDateComparator).reverse();
 
 		final SlingHttpServletResponse slingResponse = request.getSlingResponse();
 		writeJsonResponse(slingResponse, new JsonBuilder(itemList));
