e14aa33a02a5b56294fecd249e6eb10698ae336d
Paul Michelotti <pmichelotti@citytechinc.com>
2014-07-23 15:34:49 -0500

    Fun things to look at

--- harbor-proper/harbor-proper-core/src/main/java/com/citytechinc/cq/harbor/proper/core/servlets/HomePageTestServlet.java
+++ harbor-proper/harbor-proper-core/src/main/java/com/citytechinc/cq/harbor/proper/core/servlets/HomePageTestServlet.java
@@ -1,11 +1,15 @@
 package com.citytechinc.cq.harbor.proper.core.servlets;
 
 
+import com.citytechinc.cq.harbor.proper.core.services.dummypage.DummyPageService;
 import com.citytechinc.cq.library.content.request.ComponentServletRequest;
 import com.citytechinc.cq.library.servlets.AbstractComponentServlet;
 import com.day.cq.wcm.api.Page;
 import com.google.common.collect.Lists;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -20,21 +24,32 @@ import java.util.List;
 )
 public class HomePageTestServlet extends AbstractComponentServlet {
 
+    @Reference
+    private DummyPageService dummyPageService;
+
     @Override
     protected void processGet(ComponentServletRequest request) throws ServletException, IOException {
 
-        List<String> pageTitles = Lists.newArrayList();
+        writeJsonResponse(request.getSlingResponse(), dummyPageService.getDummyPageList(request.getCurrentPage()));
 
-        Iterator<Page> pageIterator = request.getCurrentPage().listChildren();
+    }
 
-        while (pageIterator.hasNext()) {
-            Page currentPage = pageIterator.next();
+    public static class DummyPage {
+        private final String title;
+        private final String updateFreq;
 
-            pageTitles.add(currentPage.getTitle());
+        public DummyPage(String title, String updateFreq) {
+            this.title = title;
+            this.updateFreq = updateFreq;
         }
 
-        writeJsonResponse(request.getSlingResponse(), pageTitles);
+        public String getTitle() {
+            return title;
+        }
 
+        public String getUpdateFreq() {
+            return updateFreq;
+        }
     }
 
 }
