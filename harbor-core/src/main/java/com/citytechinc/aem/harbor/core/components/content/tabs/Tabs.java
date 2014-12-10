2fea46c450e05e23be553c279cf579b15873c1df
Celina Macioch <cmacioch@citytechinc.com>
2014-12-10 14:03:57 -0600

    Tabs component - added support for ghost component

--- harbor-core/src/main/java/com/citytechinc/aem/harbor/core/components/content/tabs/Tabs.java
+++ harbor-core/src/main/java/com/citytechinc/aem/harbor/core/components/content/tabs/Tabs.java
@@ -1,37 +1,46 @@
 package com.citytechinc.aem.harbor.core.components.content.tabs;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import com.citytechinc.cq.component.annotations.*;
-import com.citytechinc.cq.component.annotations.widgets.Selection;
-import com.citytechinc.aem.harbor.api.constants.bootstrap.Bootstrap;
 import org.apache.sling.api.resource.Resource;
 
 import com.citytechinc.aem.bedrock.api.components.annotations.AutoInstantiate;
 import com.citytechinc.aem.bedrock.api.node.ComponentNode;
 import com.citytechinc.aem.bedrock.core.components.AbstractComponent;
-import com.citytechinc.cq.component.annotations.editconfig.ActionConfig;
+import com.citytechinc.aem.harbor.api.constants.bootstrap.Bootstrap;
 import com.citytechinc.aem.harbor.core.constants.groups.ComponentGroups;
 import com.citytechinc.aem.harbor.core.util.ComponentUtils;
+import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.ContentProperty;
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.Listener;
+import com.citytechinc.cq.component.annotations.Option;
+import com.citytechinc.cq.component.annotations.editconfig.ActionConfig;
+import com.citytechinc.cq.component.annotations.widgets.Selection;
 import com.day.cq.wcm.api.Page;
+import com.google.common.base.Predicate;
 
-@Component(value = "Tabs", group = ComponentGroups.HARBOR_SCAFFOLDING, actions = { "text: Tabs", "edit", "-", "copymove",
-	"delete", "-", "insert" }, listeners = { @Listener(name = "afterinsert", value = "REFRESH_PAGE") }, actionConfigs = {
-	@ActionConfig(xtype = "tbseparator"),
-	@ActionConfig(text = "Add Tab", handler = "function(){Harbor.Components.Tabs.addTab(this)}") }, contentAdditionalProperties = { @ContentProperty(name = "dependencies", value = "[harbor.components.content.tabs]") })
+@Component(
+    value = "Tabs",
+    group = ComponentGroups.HARBOR_SCAFFOLDING,
+    actions = { "text: Tabs", "-", "edit", "-", "copymove", "delete", "-", "insert" },
+    listeners = { @Listener(name = "afterinsert", value = "REFRESH_PAGE") }, actionConfigs = {
+        @ActionConfig(xtype = "tbseparator"),
+        @ActionConfig(text = "Add Tab", handler = "function(){Harbor.Components.Tabs.addTab(this)}")
+    },
+    contentAdditionalProperties = { @ContentProperty(name = "dependencies", value = "[harbor.components.content.tabs]") })
 @AutoInstantiate(instanceName = "tabs")
 public class Tabs extends AbstractComponent {
-	private List<Tab> tabs;
+    private List<Tab> tabs;
 
-	public static String constructUniqueId(Page page, Resource r) {
-		return ComponentUtils.getUniqueIdentifierForResourceInPage(page, r);
-	}
+    public static String constructUniqueId(Page page, Resource r) {
+        return ComponentUtils.getUniqueIdentifierForResourceInPage(page, r);
+    }
 
-	public String getName() {
-		return this.getResource().getName();
-	}
+    public String getName() {
+        return this.getResource().getName();
+    }
 
     @DialogField(fieldLabel = "Tab Type")
     @Selection(options = {
@@ -42,25 +51,36 @@ public class Tabs extends AbstractComponent {
         return get("tabType", Bootstrap.TAB_NAVIGATION_TYPE);
     }
 
-	public List<Tab> getTabs() {
-		this.tabs = new ArrayList<Tab>();
-		Iterator<ComponentNode> componentNodeIterator = this.getComponentNodes().iterator();
-		while (componentNodeIterator.hasNext()) {
-			ComponentNode currentComponentNode = componentNodeIterator.next();
+    public List<Tab> getTabs() {
+        if (tabs == null) {
+            tabs = new ArrayList<Tab>();
 
-			if (currentComponentNode.getResource().isResourceType(Tab.TYPE)) {
-				this.tabs.add(getComponent(currentComponentNode, Tab.class));
-			}
-		}
-		return this.tabs;
-	}
+            for (ComponentNode currentComponentNode : getComponentNodes(new TabPredicate())) {
+                tabs.add(getComponent(currentComponentNode, Tab.class));
+            }
+        }
+        return tabs;
+    }
 
-	public Boolean getHasTabs() {
-		return !this.getTabs().isEmpty();
-	}
+    public Boolean getHasTabs() {
+        return !this.getTabs().isEmpty();
+    }
 
-	public String getUniqueId() {
-		return constructUniqueId(getCurrentPage(), getResource());
-	}
+    public String getUniqueId() {
+        return constructUniqueId(getCurrentPage(), getResource());
+    }
 
+}
+
+final class TabPredicate implements Predicate<ComponentNode> {
+
+    @Override
+    public boolean apply(ComponentNode input) {
+        boolean isTab = false;
+        if (input != null && input.getResource() != null) {
+            final String resourceType = input.getResource().getResourceType();
+            isTab = Tab.TYPE.equals(resourceType) || "wcm/msm/components/ghost".equals(resourceType);
+        }
+        return isTab;
+    }
 }
