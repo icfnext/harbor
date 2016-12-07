228d426a5a6acaea50a2adbbcd8ce39a8b1c7919
Mark Daugherty <mark.r.daugherty@gmail.com>
2016-12-07 10:10:34 -0600

    cleanup.

--- harbor-core/src/main/java/com/icfolson/aem/harbor/core/content/page/navigation/construction/PageNavigationListConstructionStrategy.java
+++ harbor-core/src/main/java/com/icfolson/aem/harbor/core/content/page/navigation/construction/PageNavigationListConstructionStrategy.java
@@ -1,16 +1,16 @@
 package com.icfolson.aem.harbor.core.content.page.navigation.construction;
 
-import com.icfolson.aem.harbor.core.content.page.navigation.NavigablePages;
-import com.icfolson.aem.library.api.page.PageDecorator;
-import com.icfolson.aem.library.core.components.AbstractComponent;
-import com.icfolson.aem.library.models.annotations.InheritInject;
 import com.citytechinc.cq.component.annotations.DialogField;
 import com.citytechinc.cq.component.annotations.Property;
 import com.citytechinc.cq.component.annotations.widgets.NumberField;
 import com.citytechinc.cq.component.annotations.widgets.PathField;
+import com.google.common.base.Optional;
 import com.icfolson.aem.harbor.api.content.page.navigation.NavigablePage;
 import com.icfolson.aem.harbor.api.trees.construction.TreeConstructionStrategy;
-import com.google.common.base.Optional;
+import com.icfolson.aem.harbor.core.content.page.navigation.NavigablePages;
+import com.icfolson.aem.library.api.page.PageDecorator;
+import com.icfolson.aem.library.core.components.AbstractComponent;
+import com.icfolson.aem.library.models.annotations.InheritInject;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
@@ -18,60 +18,60 @@ import org.apache.sling.models.annotations.Model;
 import javax.inject.Inject;
 
 @Model(adaptables = Resource.class)
-public class PageNavigationListConstructionStrategy extends AbstractComponent implements
-        TreeConstructionStrategy<NavigablePage> {
+public class PageNavigationListConstructionStrategy extends AbstractComponent
+    implements TreeConstructionStrategy<NavigablePage> {
 
-	private Optional<NavigablePage> navigationRoot;
+    private Optional<NavigablePage> navigationRoot;
 
-	@Inject @InheritInject @Default(intValues = 1)
-	private Integer navigationDepth;
+    @Inject
+    @InheritInject
+    @Default(intValues = 1)
+    private Integer navigationDepth;
 
-	@Inject @InheritInject @Default(booleanValues = false)
-	private Boolean ignoreHideInNavigation;
+    @Inject
+    @InheritInject
+    @Default(booleanValues = false)
+    private Boolean ignoreHideInNavigation;
 
-	@Inject
-	private PageDecorator currentPage;
+    @Inject
+    private PageDecorator currentPage;
 
-	@DialogField(fieldLabel = "Navigation Root", fieldDescription = "The page at which the Navigation will start")
-	@PathField
-	public Optional<NavigablePage> getNavigationRoot() {
-		if (navigationRoot == null) {
-			PageDecorator rootPage = getRootPage();
+    @DialogField(fieldLabel = "Navigation Root", fieldDescription = "The page at which the Navigation will start")
+    @PathField
+    public Optional<NavigablePage> getNavigationRoot() {
+        if (navigationRoot == null) {
+            PageDecorator rootPage = getRootPage();
 
-			if (rootPage != null) {
-				navigationRoot = Optional.of(NavigablePages.forPageAndDepthAndChildPolicyAndCurrentPage(getRootPage(),
-						getNavigationDepth(), !getIgnoreHideInNavigation(), currentPage));
-			} else {
-				navigationRoot = Optional.absent();
-			}
-		}
+            if (rootPage != null) {
+                navigationRoot = Optional.of(NavigablePages
+                    .forPageAndDepthAndChildPolicyAndCurrentPage(rootPage, getNavigationDepth(),
+                        !getIgnoreHideInNavigation(), currentPage));
+            } else {
+                navigationRoot = Optional.absent();
+            }
+        }
 
-		return navigationRoot;
-	}
+        return navigationRoot;
+    }
 
-	protected PageDecorator getRootPage() {
-		Optional<String> rootPagePath = get("navigationRoot", String.class);
-		if (rootPagePath.isPresent()) {
-			return currentPage.getPageManager().getPage(rootPagePath.get());
-		}
+    protected PageDecorator getRootPage() {
+        return getAsPage("navigationRoot").orNull();
+    }
 
-		return null;
-	}
+    @DialogField(fieldLabel = "Navigation Depth",
+        fieldDescription = "The depth to which the navigation will search for child pages",
+        additionalProperties = { @Property(name = "emptyText", value = "1") })
+    @NumberField(allowDecimals = false, allowNegative = false, min = "1")
+    public Integer getNavigationDepth() {
+        return navigationDepth;
+    }
 
-	@DialogField(fieldLabel = "Navigation Depth", fieldDescription = "The depth to which the navigation will search for child pages", additionalProperties = { @Property(name = "emptyText", value = "1") })
-	@NumberField(allowDecimals = false, allowNegative = false, min = "1")
-	public Integer getNavigationDepth() {
-		return navigationDepth;
-	}
+    public Boolean getIgnoreHideInNavigation() {
+        return ignoreHideInNavigation;
+    }
 
-	public Boolean getIgnoreHideInNavigation() {
-		return ignoreHideInNavigation;
-	}
-
-	@Override
-	public Optional<NavigablePage> construct() {
-
-		return getNavigationRoot();
-
-	}
+    @Override
+    public Optional<NavigablePage> construct() {
+        return getNavigationRoot();
+    }
 }
