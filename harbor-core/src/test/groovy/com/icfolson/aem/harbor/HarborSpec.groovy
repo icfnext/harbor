7fe0fe73bb4652fc3593e84c4eaed14ea97aab0a
Mark Daugherty <mark.r.daugherty@gmail.com>
2017-02-28 13:24:59 -0600

    prosper and test updates.

--- harbor-core/src/test/groovy/com/icfolson/aem/harbor/HarborSpec.groovy
+++ harbor-core/src/test/groovy/com/icfolson/aem/harbor/HarborSpec.groovy
@@ -1,14 +1,17 @@
 package com.icfolson.aem.harbor
 
-import com.icfolson.aem.harbor.core.components.mixins.classifiable.Classification
 import com.icfolson.aem.library.models.specs.AemLibraryModelSpec
 import com.icfolson.aem.namespace.api.ontology.Namespace
+import com.icfolson.aem.prosper.annotations.ModelSpec
+import com.icfolson.aem.prosper.annotations.NodeTypes
 
 import static com.day.cq.tagging.TagConstants.NT_TAG
 
 /**
  * Base class for Harbor test specs.
  */
+@NodeTypes("SLING-INF/nodetypes/nodetypes.cnd")
+@ModelSpec(additionalPackages = "com.icfolson.aem.harbor.core.components.mixins.classifiable")
 abstract class HarborSpec extends AemLibraryModelSpec {
 
     def setupSpec() {
@@ -25,8 +28,5 @@ abstract class HarborSpec extends AemLibraryModelSpec {
                 }
             }
         }
-
-        // add package for classification
-        slingContext.addModelsForPackage(Classification.class.package.name)
     }
 }
