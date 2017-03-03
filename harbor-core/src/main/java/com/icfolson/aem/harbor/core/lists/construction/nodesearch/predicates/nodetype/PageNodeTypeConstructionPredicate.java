e5f886bb95675b8ce93a979db2c34bc2ffa8a5c5
Mark Daugherty <mark.r.daugherty@gmail.com>
2017-03-03 14:56:53 -0600

    updated constant value.

--- harbor-core/src/main/java/com/icfolson/aem/harbor/core/lists/construction/nodesearch/predicates/nodetype/PageNodeTypeConstructionPredicate.java
+++ harbor-core/src/main/java/com/icfolson/aem/harbor/core/lists/construction/nodesearch/predicates/nodetype/PageNodeTypeConstructionPredicate.java
@@ -1,5 +1,6 @@
 package com.icfolson.aem.harbor.core.lists.construction.nodesearch.predicates.nodetype;
 
+import com.day.cq.wcm.api.NameConstants;
 import com.google.common.base.Optional;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
@@ -7,7 +8,7 @@ import org.apache.sling.models.annotations.Model;
 @Model(adaptables = Resource.class)
 public class PageNodeTypeConstructionPredicate extends NodeTypeConstructionPredicate {
 
-    private static final Optional<String> NODE_TYPE_OPTIONAL = Optional.of("cq:Page");
+    private static final Optional<String> NODE_TYPE_OPTIONAL = Optional.of(NameConstants.NT_PAGE);
 
     public Optional<String> getNodeType() {
         return NODE_TYPE_OPTIONAL;
