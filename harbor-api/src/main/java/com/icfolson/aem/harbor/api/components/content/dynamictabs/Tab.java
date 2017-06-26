792f14a2c228474dc667027b209ae451fba48144
pmichelotti <hotarug@gmail.com>
2017-06-26 10:32:49 -0500

    a bit of housecleaning

--- harbor-api/src/main/java/com/icfolson/aem/harbor/api/components/content/dynamictabs/Tab.java
+++ harbor-api/src/main/java/com/icfolson/aem/harbor/api/components/content/dynamictabs/Tab.java
@@ -1,9 +1,5 @@
 package com.icfolson.aem.harbor.api.components.content.dynamictabs;
 
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Model;
-
-//@Model(adaptables = Resource.class)
 public interface Tab {
 
     String RESOURCE_TYPE = "harbor/components/content/dynamictabs/tab";
