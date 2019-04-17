badd998f72e3c3fa9523c04d967e93639f2c0b0b
Eric Van Geem <eric.vangeem@icfnext.com>
2019-04-17 16:53:02 -0500

    update to aem-library 12.0.0 for AEM 6.5 compatibility.

--- harbor-core/src/main/java/com/icfolson/aem/harbor/core/components/content/dynamicaccordion/v1/NewAccordionItemDataSource.java
+++ harbor-core/src/main/java/com/icfolson/aem/harbor/core/components/content/dynamicaccordion/v1/NewAccordionItemDataSource.java
@@ -2,9 +2,14 @@ package com.icfolson.aem.harbor.core.components.content.dynamicaccordion.v1;
 
 import com.icfolson.aem.harbor.api.components.content.dynamicaccordion.DynamicAccordionItem;
 import com.icfolson.aem.harbor.core.components.dynamics.v1.AbstractDynamicItemDataSource;
-import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.servlets.ServletResolverConstants;
+import org.osgi.service.component.annotations.Component;
 
-@SlingServlet(resourceTypes = NewAccordionItemDataSource.RESOURCE_TYPE)
+import javax.servlet.Servlet;
+
+@Component(service = Servlet.class, property = {
+    ServletResolverConstants.SLING_SERVLET_RESOURCE_TYPES + "=" + NewAccordionItemDataSource.RESOURCE_TYPE
+})
 public class NewAccordionItemDataSource extends AbstractDynamicItemDataSource {
 
     public static final String RESOURCE_TYPE = NewAccordionItem.RESOURCE_TYPE + "/options";
